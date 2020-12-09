9cbe61637e5dc7dda8c8c6f351f404fecba3b684
Jack Hubert <jphubert93@gmail.com>
2020-12-09 14:37:34 -0700

    commit tree 682a26f9fca55f1f7714ed992b47f1dfa0d3cec4

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,8 @@
+require 'pry'
 # Create method `roll` that returns a random number between 1 and 6
+def roll
+  rand(1..6)
+end
 
 # Feel free to google "how to generate a random number in ruby"
 
-def roll
-  # code goes here
-end
